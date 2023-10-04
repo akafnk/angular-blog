ef5719bec8bbfbeb586592a14e14609c99b95e4a
Frank Gárate <frank.garaterios@gmail.com>
2023-10-04 16:04:06 -0300

    Mudanças no CSS, cards e conteudo do blog

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,53 @@
 export const dataFake = [
   {
-    "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
-  },
-  {
-    "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
-  }
+    "id": "1",
+    "title": "VALE A PENA APRENDER ANGULAR",
+    "description": "A resposta para essas perguntas é sim, vale a pena aprender Angular. Esse framework é muito popular, e, por isso, há uma grande comunidade online disposta a compartilhar os seus conhecimentos e auxiliar nas dúvidas dos iniciantes. Existe muito material de estudo entre livros, tutoriais, apostilas e cursos online para facilitar o seu aprendizado.",
+    "photoCover":"https://www.angularjswiki.com/angular/use-ngoptimizedimage-to-improve-image-loading-performance-in-angular/featured.png"
+},
+{
+    "id": "2",
+    "title": "Por que React não é um framework?",
+    "description": "Um framework possui regras e organizações de arquivos restritos em seus códigos. Já o React, por ser uma biblioteca, não implementa essas restrições, mas sim, adiciona novas funcionalidades a algo já existente, tornando flexível e adaptável a qualquer projeto.",
+    "photoCover":"https://th.bing.com/th/id/R.80c5007eb1c89d61a8ea9fd04ab11503?rik=%2bTQiq1dMQ2qOHw&pid=ImgRaw&r=0"
+},
+{
+    "id": "3",
+    "title": "JavaScript",
+    "description": "JavaScript é uma linguagem de script orientada a objetos, multiplataforma. É uma linguagem pequena e leve. Dentro de um ambiente de host.",
+    "photoCover": "https://marquesfernandes.com/wp-content/uploads/2020/01/1555172.jpg"
+},
+{
+    "id": "4",
+    "title": "HTML",
+    "description": "Linguagem de Marcação de Hipertexto é o componente básico da web, ele permite inserir o conteúdo e estabelecer a estrutura básica de um website.",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0ghZhvvXNDgKC2Gqh3npLdNuXyBvGyqxkxg&usqp=CAU"
+},
+{
+    "id": "5",
+    "title": "CSS",
+    "description": "CSS é chamado de linguagem Cascading Style Sheet e é usado para estilizar elementos escritos em uma linguagem de marcação como HTML. O CSS separa o conteúdo da representação visual do site.  ",
+    "photoCover": "https://blog.logrocket.com/wp-content/uploads/2023/01/css-full-page-background.png"
+},
+{
+    "id": "6",
+    "title": "JAVA",
+    "description": "Java é um tipo de linguagem de programação criada e comercializada pela Sun Microsystems desde 1995. É definida como uma linguagem orientada a objetos. Sua intenção é permitir que os desenvolvedores escrevam o programa apenas uma vez e o executem por meio de qualquer dispositivo.",
+    "photoCover": "https://th.bing.com/th/id/R.d2d71ce296d5197285643c703ed38d2e?rik=5Fb3tpdX2%2bkx7A&pid=ImgRaw&r=0"
+},
+{
+    "id": "7",
+    "title": "PYTHON",
+    "description": "Python é uma linguagem de programação de alto nível, interpretada de script, imperativa, orientada a objetos, funcional, de tipagem dinâmica e forte.",
+    "photoCover": "https://th.bing.com/th/id/R.9918b61459ffe045aa95dd55628ba62d?rik=oTlwfQZkD0JpjA&pid=ImgRaw&r=0"
+}
+
 ]
+
+
+
+
+
+
+
+
